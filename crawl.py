7ead79788ebd90c80377b67c800a85894da21b67
Ed Summers <ehs@pobox.com>
2010-11-26 12:18:46 -0500

    send a better user agent to let data.gov.uk know who is requesting the pages. also do not stop when encountering http error.

--- crawl.py
+++ crawl.py
@@ -7,15 +7,19 @@ Crawl the rdfa at data.gov.uk and store them away in a BerkeleyDB triplestore.
 import re
 import urllib
 
-from rdflib.graph import ConjunctiveGraph
-from rdflib.term import URIRef
+import rdflib
 
-graph = ConjunctiveGraph('Sleepycat')
+# set the user agent so that data-gov-uk will know who we are
+rdflib.parser.headers = {"User-agent": "data-gov-uk-harvester <https://github.com/edsu/data-gov-uk-harvester>"}
+
+graph = rdflib.ConjunctiveGraph('Sleepycat')
 graph.open('store', create=True)
 
+# the paged package listing that we will crawl to discover dataset urls
 page_url_tmpl = "http://data.gov.uk/search/apachesolr_search/?filters=type:ckan_package&page=%s"
-page = 0
+page = 444
 
+# extract rdf from each dataset html/rdfa page
 while True:
     page += 1
     page_url = page_url_tmpl % page
@@ -26,13 +30,16 @@ while True:
     for dataset_url in re.findall(r'"(http://data.gov.uk/dataset/.*?)"', html):
         found += 1
         print "fetching dataset %s" % dataset_url
-        graph.parse(location=dataset_url, format='rdfa', lax=True)
+        try:
+            graph.parse(location=dataset_url, format='rdfa', lax=True)
+        except Exception, e:
+            print e
 
     if found == 0:
         break
 
 # no sense in keeping tons of css stylesheet assertions is there?
-for t in graph.triples((None, URIRef('http://www.w3.org/1999/xhtml/vocab#stylesheet'), None)):
+for t in graph.triples((None, rdflib.URIRef('http://www.w3.org/1999/xhtml/vocab#stylesheet'), None)):
     graph.remove(t)
 
 graph.serialize(open('data.rdf', 'w'))
